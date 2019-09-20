19f70ef86e7928c9eb8ffcc311ab724b21588043
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-20 11:03:15 +0100

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
 import WelcomePage from './components/WelcomePage'
+import CharacterList from "./components/CharacterList.js";
+import { Route } from 'react-router-dom';
 import axios from 'axios';
 
 
 export default function App() {
-  const [characters, setCharacters] = useState([])
-	useEffect(() => {
-		// axios.get('https://rickandmortyapi.com/api/character/')
-		.then(res => {
-      // debugger
-      setCharacters(res.data.results);
-      console.log(res.data.results)
-		})
-		.catch(error => {
-      debugger
-      console.error(error)})
-		return () => {
-			console.log('cleanup')
-		};
-	}, [])
+  
   return (
     <main>
-      {people
-      ? 
-      people.map(person => <Person data={person} key={person.url} />)
-      :
-       null}
-      <Header />
-      <WelcomePage/>
-    </main>
+			<Header />
+			<Route exact path="/" component={WelcomePage} />
+			<Route path="/characters" component={CharacterList} />		
+		</main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,17 +1,53 @@
 import React, { useEffect, useState } from "react";
-
+import SearchForm from "./SearchForm";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
 export default function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        setCharacters(res.data.results);
+      })
+      .catch(error => console.error(error));
+    return () => {
+      setCharacters([]);
+    };
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
-  
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    let searchQuery =
+      "?" +
+      Object.keys(search)
+        .map(key => key + "=" + search[key])
+        .join("&");
+    axios
+      .get(`https://rickandmortyapi.com/api/character/${searchQuery}`)
+      .then(res => {
+        setCharacters(res.data.results);
+      })
+      .catch(error => console.error(error));
+    return () => {
+      setCharacters([]);
+    };
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  }, [search.name, search.gender, search.species, search.status, search.type]);
 
+  const characterList = searchResults.length > 0 ? searchResults : characters;
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm search={search} setSearch={setSearch} />
+      <h2>Character List</h2>
+      {characterList.map(character => (
+        <CharacterCard key={character.id} data={character} />
+      ))}
     </section>
   );
 }
